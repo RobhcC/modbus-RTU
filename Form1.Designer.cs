ef79b86c7dc921c06be4a77baa79d18ccdbb964e
Administrator <3280668733@qq.com>
2026-04-07 17:32:23 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WinformPractise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSendData = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCloseSerial = new System.Windows.Forms.Button();
+            this.btnOpenSerial = new System.Windows.Forms.Button();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.cboPortName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvModbus = new System.Windows.Forms.DataGridView();
+            this.timerReceive = new System.Windows.Forms.Timer(this.components);
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModbus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lstLog, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgvModbus, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(668, 459);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lstLog
+            // 
+            this.lstLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.ItemHeight = 12;
+            this.lstLog.Location = new System.Drawing.Point(3, 162);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(662, 154);
+            this.lstLog.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnClearLog);
+            this.groupBox2.Controls.Add(this.btnSendData);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 94);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(662, 62);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "报文发送与清除日志";
+            // 
+            // btnSendData
+            // 
+            this.btnSendData.Location = new System.Drawing.Point(8, 20);
+            this.btnSendData.Name = "btnSendData";
+            this.btnSendData.Size = new System.Drawing.Size(75, 23);
+            this.btnSendData.TabIndex = 3;
+            this.btnSendData.Text = "发送报文";
+            this.btnSendData.UseVisualStyleBackColor = true;
+            this.btnSendData.Click += new System.EventHandler(this.btnSendData_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnCloseSerial);
+            this.groupBox3.Controls.Add(this.btnOpenSerial);
+            this.groupBox3.Controls.Add(this.cboBaudRate);
+            this.groupBox3.Controls.Add(this.cboPortName);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(662, 85);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "串口配置";
+            // 
+            // btnCloseSerial
+            // 
+            this.btnCloseSerial.Location = new System.Drawing.Point(269, 46);
+            this.btnCloseSerial.Name = "btnCloseSerial";
+            this.btnCloseSerial.Size = new System.Drawing.Size(75, 23);
+            this.btnCloseSerial.TabIndex = 5;
+            this.btnCloseSerial.Text = "关闭串口";
+            this.btnCloseSerial.UseVisualStyleBackColor = true;
+            this.btnCloseSerial.Click += new System.EventHandler(this.btnCloseSerial_Click);
+            // 
+            // btnOpenSerial
+            // 
+            this.btnOpenSerial.Location = new System.Drawing.Point(268, 15);
+            this.btnOpenSerial.Name = "btnOpenSerial";
+            this.btnOpenSerial.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenSerial.TabIndex = 4;
+            this.btnOpenSerial.Text = "打开串口";
+            this.btnOpenSerial.UseVisualStyleBackColor = true;
+            this.btnOpenSerial.Click += new System.EventHandler(this.btnOpenSerial_Click);
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(54, 49);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(121, 20);
+            this.cboBaudRate.TabIndex = 3;
+            this.cboBaudRate.SelectedIndexChanged += new System.EventHandler(this.cboBaudRate_SelectedIndexChanged);
+            // 
+            // cboPortName
+            // 
+            this.cboPortName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPortName.FormattingEnabled = true;
+            this.cboPortName.Location = new System.Drawing.Point(54, 18);
+            this.cboPortName.Name = "cboPortName";
+            this.cboPortName.Size = new System.Drawing.Size(121, 20);
+            this.cboPortName.TabIndex = 2;
+            this.cboPortName.SelectedIndexChanged += new System.EventHandler(this.cboPortName_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "波特率";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "串口号";
+            // 
+            // dgvModbus
+            // 
+            this.dgvModbus.AllowUserToAddRows = false;
+            this.dgvModbus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvModbus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvModbus.Location = new System.Drawing.Point(3, 322);
+            this.dgvModbus.Name = "dgvModbus";
+            this.dgvModbus.ReadOnly = true;
+            this.dgvModbus.RowTemplate.Height = 23;
+            this.dgvModbus.Size = new System.Drawing.Size(662, 134);
+            this.dgvModbus.TabIndex = 4;
+            // 
+            // timerReceive
+            // 
+            this.timerReceive.Interval = 1000;
+            this.timerReceive.Tick += new System.EventHandler(this.timerReceive_Tick);
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Location = new System.Drawing.Point(268, 20);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btnClearLog.TabIndex = 4;
+            this.btnClearLog.Text = " 清除日志";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 459);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModbus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCloseSerial;
+        private System.Windows.Forms.Button btnOpenSerial;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.ComboBox cboPortName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendData;
+        private System.Windows.Forms.Timer timerReceive;
+        private System.Windows.Forms.DataGridView dgvModbus;
+        private System.Windows.Forms.Button btnClearLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformPractise
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void cboPortName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Name = cboPortName.Text;
+            AddLog($"将串口更改为{Name}");
+        }
+
+        private void cboBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string BaudRate = cboBaudRate.Text;
+            AddLog($"将波特率改为{BaudRate}");
+        }
+        private void InitModbusTable() 
+        {
+           dgvModbus.Columns.Clear();//自动清除列防止程序多次启动不断增加列导致报错
+           dgvModbus.Rows.Clear();//清除行
+
+            //手动创建列
+            dgvModbus.Columns.Add("Addr","寄存器地址");
+            dgvModbus.Columns.Add("Name","寄存器名称");
+            dgvModbus.Columns.Add("Value", "数据值");
+            dgvModbus.Columns.Add("Status", "设备状态");
+            //手动创建行
+            dgvModbus.Rows.Add("0x00", "温度值", "--", "正常");
+            dgvModbus.Rows.Add("0x01", "湿度值", "--", "正常");
+            dgvModbus.Rows.Add("0x02", "运行状态", "--", "Run");
+
+            dgvModbus.AutoSizeColumnsMode=DataGridViewAutoSizeColumnsMode.Fill;//自动设置最佳列宽，防止“黑框”剩余
+            dgvModbus.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;//行宽不作处理(无Fill方法)
+            dgvModbus.BackgroundColor = this.BackColor;//让背景色跟窗体一致
+
+            // 动态计算行高
+            if (dgvModbus.Rows.Count > 0)
+            {
+                // 表格总高度 - 表头高度 = 所有行可分配的总高度
+                int available_height = dgvModbus.Height - dgvModbus.ColumnHeadersHeight;
+                // 平均分配给每一行
+                int row_height = available_height / dgvModbus.Rows.Count;
+
+                // 立即设置现有行的高度
+                foreach (DataGridViewRow row in dgvModbus.Rows)
+                {
+                    row.Height = row_height;
+                }
+                // 后续新增行也会自动使用这个高度
+                dgvModbus.RowTemplate.Height = row_height;
+            }
+            
+
+        }
+        private byte[] BuildModbusReadCommand()
+        {
+            byte[] command = new byte[6];
+            command[0] = 0x01;
+            command[1] = 0x03;
+            command[2] = 0x00;
+            command[3] = 0x00;
+            command[4] = 0x00;
+            command[5] = 0x02;
+
+            byte[] crc = CalculateCRC16(command);
+            byte[] fullCommand = new byte[8];
+            Array.Copy(command, fullCommand, 6);
+            fullCommand[6] = crc[0];
+            fullCommand[7] = crc[1];
+
+            return fullCommand;
+        }
+
+        private void UpdateModbusTable(double temp, double humi,string status,Color color) 
+        {
+            if (dgvModbus.Rows.Count < 3) return;
+            dgvModbus.Rows[0].Cells["Value"].Value = temp.ToString("0.0");
+            dgvModbus.Rows[1].Cells["Value"].Value = humi.ToString("0.0");
+            dgvModbus.Rows[2].Cells["Value"].Value = status;
+            dgvModbus.Rows[2].DefaultCellStyle.BackColor = color;
+            dgvModbus.Rows[2].DefaultCellStyle.ForeColor = Color.White;
+        }
+
+        private void ClearModbusTable()
+        {
+            if (dgvModbus.Rows.Count < 3) return;
+            // 恢复温度、湿度为--
+            dgvModbus.Rows[0].Cells["Value"].Value = "--";
+            dgvModbus.Rows[1].Cells["Value"].Value = "--";
+            // 恢复运行状态默认值
+            dgvModbus.Rows[2].Cells["Value"].Value = "Run";
+            dgvModbus.Rows[2].DefaultCellStyle.BackColor = this.BackColor;
+            dgvModbus.Rows[2].DefaultCellStyle.ForeColor = Color.Black;
+        }
+
+        //手动计算CRC16校验 
+        private byte[] CalculateCRC16(byte[] data) 
+        {
+          ushort crc = 0xFFFF;
+            for (int i = 0; i < data.Length; i++) //遍历需要校验的字节
+            {
+             crc ^= data[i];//异或运算
+                for (int j = 0; j < 8; j++)//8次位移运算 
+                {
+                    if ((crc & 1) > 0)//判断最后一位是否为1
+                    {   //右移一位再异或多项式
+                        crc >>= 1;
+                        crc ^= 0xA001;
+
+                    }
+                    else 
+                    {
+                     crc>>= 1;//为0只右移一位
+                    }                           
+                }        
+            }
+
+            return new byte[] { (byte)crc, (byte)(crc >> 8) };//校验码低八位在前，高八位在后
+        }
+        private void AddLog(string msg) //使用日志
+        {
+            string time = DateTime.Now.ToString("yyyy/MM/dd:HH:mm:ss");
+            lstLog.Items.Add($"{time}{msg}");
+            lstLog.TopIndex = lstLog.Items.Count - 1;
+        }
+
+        
+        //窗体加载自启动
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cboPortName.Items.AddRange(ports);
+            cboBaudRate.Items.AddRange(new string[] { "9600", "19200", "38400" });
+            cboBaudRate.Text = "9600";
+
+            btnCloseSerial.Enabled = false;
+            btnSendData.Enabled = false;
+            btnOpenSerial.Enabled = true;
+            
+
+            InitModbusTable();
+        }
+
+        private void btnOpenSerial_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(cboPortName.Text))
+            {
+                AddLog("【提示】请选择串口号");
+                return;
+            }
+            if (serialPort.IsOpen)
+            {
+                AddLog($"{serialPort.PortName}【提示】串口已打开，请勿重复操作");
+                return;
+            }
+            try
+            {
+                int baudRate = Convert.ToInt32(cboBaudRate.Text);//字符串转为整数
+                string comName = cboPortName.Text;
+                //串口配置
+                serialPort.BaudRate = baudRate;
+                serialPort.PortName = comName;
+                serialPort.DataBits = 8;
+                serialPort.Parity = Parity.None;
+                serialPort.StopBits = StopBits.One;
+
+                serialPort.PortName = cboPortName.Text;
+                serialPort.Open();
+                AddLog($"【成功】{serialPort.PortName}串口已打开");
+                timerReceive.Start();
+
+                cboPortName.Enabled = false;
+                cboBaudRate.Enabled = false;
+                btnCloseSerial.Enabled = true;
+                btnOpenSerial.Enabled = false;
+                btnSendData.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                AddLog($"【错误】串口打开失败{ex.Message}");
+            }
+        }
+
+        private void btnCloseSerial_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                    AddLog($"【成功】{serialPort.PortName}串口已关闭");
+                    timerReceive.Stop();
+                }
+                else
+                {
+                    AddLog($"【提示】{serialPort.PortName}串口已关闭，请勿重复操作！");
+                }
+            }
+            catch (Exception ex)
+            {
+                AddLog($"【错误】{serialPort.PortName}串口关闭失败！{ex.Message}");
+            }
+            finally
+            {
+                cboPortName.Enabled = true;
+                cboBaudRate.Enabled= true;
+                btnCloseSerial.Enabled = false;
+                btnOpenSerial.Enabled = true;
+                btnSendData.Enabled = false;
+            }
+        }
+
+        private void btnSendData_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                AddLog("【提示】请先打开串口");
+                return;
+            }
+
+            try
+            {
+                byte[] modbusCmd = BuildModbusReadCommand();
+                serialPort.Write(modbusCmd, 0, modbusCmd.Length);//0：从报文的第一个字节开始发发到最后一个modbusLength
+                AddLog($"{serialPort.PortName}发送报文" + BitConverter.ToString(modbusCmd).Replace("-", " "));
+            }
+            catch (Exception ex)
+            {
+                AddLog($"【错误】发送失败{ex.Message}");
+            }
+        }
+
+        private void timerReceive_Tick(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen) return;
+
+            try
+            {
+                if (serialPort.BytesToRead <= 0) return;// 1只有缓冲区有数据才处理
+
+                byte[] allData = new byte[serialPort.BytesToRead]; //  一次性读取所有缓冲区数据，避免多次读取导致的粘包
+                serialPort.Read(allData, 0, allData.Length);
+
+                int index = 0;
+                while (index <= allData.Length - 9)// 循环查找完整的9字节Modbus报文（精准匹配报文头01 03）
+                {
+                    if (allData[index] == 0x01 && allData[index + 1] == 0x03)// 找到Modbus标准报文头：从站地址01 + 功能码03
+                    {
+                        byte[] buffer = new byte[9];// 提取完整的9字节有效报文
+                        Array.Copy(allData, index, buffer, 0, 9);
+
+                        AddLog($"{serialPort.PortName}接收报文：" + BitConverter.ToString(buffer).Replace("-", ""));
+
+                        byte[] data = new byte[7];
+                        Array.Copy(buffer, 0, data, 0, 7);
+
+                        byte[] calcCrc = CalculateCRC16(data);
+                        byte recvCrcLow = buffer[7];
+                        byte recvCrcHigh = buffer[8];
+
+                        AddLog($"计算CRC：{calcCrc[0]:X2} {calcCrc[1]:X2} | 接收CRC：{recvCrcLow:X2} {recvCrcHigh:X2}");// 打印CRC对比，彻底透明化
+
+                        // CRC校验
+                        if (calcCrc[0] == recvCrcLow && calcCrc[1] == recvCrcHigh)
+                        {
+                            // 校验通过，解析数据
+                            if (buffer[2] == 0x04)
+                            {
+                                short tempRaw = (short)(buffer[3] << 8 | buffer[4]);
+                                double temperature = tempRaw / 10.0;
+
+                                string status;
+                                Color color;
+                                if (temperature > 35)
+                                {
+                                    status = "高温预警";
+                                    color = Color.Red;
+                                }
+                                else if (temperature < 0)
+                                {
+                                    status = "低温异常";
+                                    color = Color.Orange;
+                                }
+                                else
+                                {
+                                    status = "正常";
+                                    color = Color.Green;
+                                }
+
+                                ushort humiRaw = (ushort)(buffer[5] << 8 | buffer[6]);
+                                double humidity = humiRaw / 10.0;
+
+                                // 更新界面表格
+                                UpdateModbusTable(temperature, humidity, status, color);
+                                AddLog("【校验成功】数据解析完成！");
+                            }
+                        }
+                        else
+                        {
+                            AddLog("【CRC校验失败】报文损坏，已丢弃！");
+                            ClearModbusTable(); 
+                        }
+                        index += 9;// 处理完所有数据，清空缓冲区，避免残留数据影响下一次接收
+                    }
+                    else
+                    {
+                        index++; // 不是报文头，往后偏移1位继续找
+                    }
+                }
+                serialPort.DiscardInBuffer();// 处理完所有数据，清空缓冲区，避免残留数据影响下一次接收
+            }
+            catch (Exception ex)
+            {
+                AddLog($"【错误】{ex.Message}");
+                ClearModbusTable();
+                serialPort.DiscardInBuffer();
+            }
+        }
+
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            lstLog.Items.Clear();
+            AddLog("操作日志已清除");
+        }
+    }
+}
